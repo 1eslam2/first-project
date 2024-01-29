3ab656235f13967dd8a94d203d06a477d3a1babc
1eslam2 <158002610+1eslam2@users.noreply.github.com>
2024-01-29 22:52:19 +0200

    Add files via upload

---
+++ python/first.py
@@ -0,0 +1,23 @@
+# q1
+x = "eslam ashraf salah"
+y = 20
+print(f"my name is {x} and my age is {y}year")
+# q2
+x = 1 + 2j
+im = x.imag
+re = x.real
+print(f"the imaginary part is:{im}")
+print(f"the real part is :{re}")
+# q3
+x = 10
+y = 159.650
+z = 55
+x = float(x)
+print(f"value of x is :{x:.10f}")
+print("type of x is :" + str(type(x)))
+y=int(y)
+print(f"value of y is :{y}")
+print("type of y is :" + str(type(y)))
+z=complex(z)
+print(f"value of z is :{z}")
+print("type of z is :"+str(type(z)))
